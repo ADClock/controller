b9e26c2b7bca517b0a42a016f2eecb1d36fed6c8
Paule <44635962+V3lop5@users.noreply.github.com>
2022-10-26 17:12:33 +0200

    Add tests to authentication endpoints

--- tests/api/test_authentication.py
+++ tests/api/test_authentication.py
@@ -21,7 +21,7 @@ def test_register_endpoint(client: TestClient, db: Session):
     assert db_user
 
 
-def test_register_twice_endpoint(client: TestClient):
+def test_register_endpoint_existing_user(client: TestClient):
     payload = get_register_payload()
     r = client.post("/auth/register", json=payload)
     new_user = r.json()
@@ -31,3 +31,49 @@ def test_register_twice_endpoint(client: TestClient):
 
     r2 = client.post("/auth/register", json=payload)
     assert r2.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_login_endpoint(client: TestClient):
+    payload = get_register_payload()
+    r = client.post("/auth/register", json=payload)
+    new_user = r.json()
+    assert r.status_code == status.HTTP_200_OK
+    assert new_user
+    assert new_user['username'] == payload['username']
+
+    r2 = client.post("/auth/login", data=payload)
+    assert r2.status_code == status.HTTP_200_OK
+    assert r2.json()['access_token']
+
+
+def test_login_endpoint_unknown_user(client: TestClient):
+    payload = get_register_payload()
+    r = client.post("/auth/login", data=payload)
+    assert r.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_test_token_endpoint_without_token(client: TestClient):
+    r = client.post("/auth/test-token")
+    assert r.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_test_token_endpoint(client: TestClient):
+    payload = get_register_payload()
+    r = client.post("/auth/register", json=payload)
+    new_user = r.json()
+    assert r.status_code == status.HTTP_200_OK
+    assert new_user
+    assert new_user['username'] == payload['username']
+
+    r2 = client.post("/auth/login", data=payload)
+    assert r2.status_code == status.HTTP_200_OK
+    assert r2.json()['access_token']
+
+    r3 = client.post("/auth/test-token", headers={"Authorization": f"Bearer {r2.json()['access_token']}"})
+    assert r3.status_code == status.HTTP_200_OK
+    assert r3.json()['username'] == payload['username']
+
+
+def test_test_token_endpoint_with_invalid_token(client: TestClient):
+    r = client.post("/auth/test-token", headers={"Authorization": "Bearer invalid"})
+    assert r.status_code == status.HTTP_403_FORBIDDEN
