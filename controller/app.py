2ad7c3fd2f55060e7745413dd88c44eb019e9832
Paule <44635962+V3lop5@users.noreply.github.com>
2022-10-26 15:17:43 +0200

    Add ADClock simulation ticker to FastAPI startup

--- controller/app.py
+++ controller/app.py
@@ -1,5 +1,8 @@
+import asyncio
+
 from fastapi import FastAPI
 
+from controller.adclock import adclock
 from controller.api import api_router
 from controller.core import settings
 from controller.database import init_db
@@ -14,3 +17,9 @@ app.include_router(api_router)
 def init_database():
     init_db.check_connection()
     init_db.create_all()
+
+
+# Start simulation
+@app.on_event("startup")
+async def run_simulation():
+    asyncio.create_task(adclock.run_simulation())
