46b07759374a393d27495b3a98199666cf39c639
Paule <44635962+V3lop5@users.noreply.github.com>
2022-10-27 11:54:53 +0200

    Reformat code for default token expire timedelta

--- controller/core/security.py
+++ controller/core/security.py
@@ -13,14 +13,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def create_access_token(
         subject: Union[str, Any],
-        expires_delta: timedelta = None
+        expires_delta: timedelta = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 ) -> str:
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
+    expire = datetime.utcnow() + expires_delta
     to_encode = {"exp": expire, "sub": str(subject)}
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
